7bdfc79b7302893c1d5ff42afcfac3eb6c9f76e1
hankakukatakana <masaki.0409.a@gmail.com>
2022-10-29 17:50:31 +0900

    kore

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Post(db.Model):
 
 @app.route("/")
 def home():
-    return render_template ("home.html")
+    return render_template ("index.html")
 
 @app.route("/<name>")
 def coin(name): 
